799c69e89be1d860ba46fc59a452f8ba697a3cb5
Taylor Choque <taylor69gamer@gmail.com>
2025-11-20 12:36:49 -0500

    jv

--- javascrip, pag, 04.js
+++ javascrip, pag, 04.js
@@ -1,23 +1,23 @@
-console.registro("Hola Mundo");
+console.log("Hola Mundo");
 alert("Hola Mundo, esto es una alerta")
 
 function triste(){
     document.getElementById('carita').src='imagenes/caritaTriste 02.png';
-    alert("Por que tan triste mano")
+    alert("Por que tan triste mano");
 }
 function feliz(){
     document.getElementById('carita').src='imagenes/caritaFelíz 01.png';
 }
 function cambiarColor(){
-    document.getElementById('menu').src.color='#E8BEAC'
+    document.getElementById('menu').src.color='#E8BEAC';
 
 }
 function cambiarLogo(){
-    document.getElementById('logo').src='imagenes/Logo Inicial.JPG'
+    document.getElementById('logo').src='imagenes/Logo Inicial.JPG';
 }
 function cambiarFondo(){
-    document.getElementById('menu')style.background='#524604'
+    document.getElementById('menu').style.background='#524604';
 }
 function cambiarTexto(){
-    document.getElementById('Subtitulo').textContent="YA LE SE AL 35"
+    document.getElementById('Subtitulo').textContent="YA LE SE AL 35";
 }
